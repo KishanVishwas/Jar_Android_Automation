c1eb0466f8ac219c9f654e966dd806a333f73c43
Karthikhk1720 <karthikhk172000@gmail.com>
2025-11-12 16:14:36 +0530

    git locators

--- src/main/Java/pageObjectModel/onboardingFlow.java
+++ src/main/Java/pageObjectModel/onboardingFlow.java
@@ -1,6 +1,7 @@
 package pageObjectModel;
 
 import io.appium.java_client.AppiumBy;
+import locaters.login_page;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -9,23 +10,48 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import static basePackage.actions.takeScreenShot;
 import static basePackage.driverFactory.driver;
 import static basePackage.driverFactory.wait;
+import static locaters.login_page.*;
 
-public class onboardingFlow {
+
+public class onboardingFlow  {
 
     public void langaugeSelection(){
         try {
-            wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.widget.TextView[@text=\"Choose the app language you prefer.\"]")));
-           WebElement english= wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.TextView[@text=\"English\"]")));
+            wait.until(ExpectedConditions.presenceOfElementLocated(langaugePageText));
+           WebElement english= wait.until(ExpectedConditions.elementToBeClickable(English));
            if (english.isDisplayed()){
                System.out.println("English langauge is visible and selecting");
                english.click();
            }
-           wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View[3]/android.view.View/android.view.View/android.view.View[2]"))).click();  // clicking on Apply cta after selecting ENglish language
+           wait.until(ExpectedConditions.elementToBeClickable(applylangauge)).click();  // clicking on Apply cta after selecting ENglish language
         }
         catch (NoSuchElementException e) {
-
             takeScreenShot(driver,"Language selection screen");
         }
-
     }
+    public void onboardingVidio(){
+      try{
+       wait.until(ExpectedConditions.elementToBeClickable(skipOnboardingvideo)).click();
+       wait.until(ExpectedConditions.visibilityOf((WebElement) startSavingJoueyCTA)).click();
+          System.out.println("Onboarding video as played");
+        }
+      catch (NoSuchElementException e){
+          System.out.println("Onboarding video is not playing");
+        }
+
+      if (driver.findElement(welcomeBackText).isDisplayed()){
+          driver.findElement(useOtherNumberCTA).click();
+      }
+        WebElement NonOfAbove = driver.findElement(nonOfTheAbove);
+        if (NonOfAbove.isDisplayed()) {
+            NonOfAbove.click();
+        }
+    }
+    public void userLogin(){
+        WebElement NonOfAbove = driver.findElement(nonOfTheAbove);
+        if (NonOfAbove.isDisplayed()) {
+            NonOfAbove.click();
+        }
+    }
+
 }
