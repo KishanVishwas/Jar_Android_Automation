8a79726452abab2b9191e59aab6249ed17d1e43f
KishanVishwas <kishanshetty312@gmail.com>
2025-11-19 11:35:38 +0530

    arranged the emulator and simulators, flow arranges

--- src/main/Java/basePackage/driverFactory.java
+++ src/main/Java/basePackage/driverFactory.java
@@ -1,43 +1,89 @@
 package basePackage;
 
+import utilsPackage.configReader;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
 public class driverFactory {
-    public static WebDriverWait wait;
-    public static AppiumDriver driver = null;
 
-    public static AppiumDriver driverSetup() throws MalformedURLException {
+    public static WebDriverWait wait;
+    public static AppiumDriver driver;
+
+    // -------------------------------------------------------
+    //     MAIN DRIVER SETUP (Platform Switch)
+    // -------------------------------------------------------
+    public static AppiumDriver driverSetup() throws Exception {
+
+        String platform = configReader.get("androidplatformName");
+
+        if (platform.equalsIgnoreCase("Android")) {
+            driver = setupAndroid();
+        }
+        else if (platform.equalsIgnoreCase("iOS")) {
+            driver = setupIOS();
+        }
+        else {
+            throw new Exception("❌ Invalid platform! Use: android / ios");
+        }
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        return driver;
+    }
+
+    // -------------------------------------------------------
+    //                   ANDROID SETUP
+    // -------------------------------------------------------
+    private static AppiumDriver setupAndroid() throws Exception {
+
         DesiredCapabilities caps = new DesiredCapabilities();
 
-        // --- Android Device Capabilities ---
         caps.setCapability("platformName", "Android");
         caps.setCapability("appium:automationName", "UiAutomator2");
-        caps.setCapability("appium:deviceName", "Samsung Note 10");
-        caps.setCapability("appium:udid", "R3CMA02PYNK");
-        caps.setCapability("appium:platformVersion", "12.0");
-        caps.setCapability("appium:autoAcceptAlerts", true);
 
-        // --- App Specific Capabilities ---
-        caps.setCapability("appium:app", "/Users/kishanr/Downloads/base (1).apk");
-        caps.setCapability("appium:appPackage", "com.aso_centric.jar.staging");
-        caps.setCapability("appium:appActivity", "com.jar.app.feature.home.ui.activity.HomeActivity");
-        caps.setCapability("appium:appWaitActivity", "com.jar.app.*");
-        caps.setCapability("appium:noReset", false);
-        caps.setCapability("appium:fullReset", true);
+        // You can load these also from config.xml if needed
+        caps.setCapability("appium:deviceName", configReader.get("androidDeviceName"));
+        caps.setCapability("appium:udid", configReader.get("androidUDID"));
+        caps.setCapability("appium:platformVersion", configReader.get("androidPlatformVersion"));
+
         caps.setCapability("appium:autoGrantPermissions", true);
 
-        // --- Connect to Appium Server ---
-        URL url = new URL("http://127.0.0.1:4723/");
+        caps.setCapability("appium:app", configReader.get("androidApp"));
+        caps.setCapability("appium:appPackage", configReader.get("androidPackage"));
+        caps.setCapability("appium:appActivity", configReader.get("androidActivity"));
 
-        driver = new AndroidDriver(url, caps);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        return driver;
+        return new AndroidDriver(new URL(configReader.get("appiumServerURL")), caps);
+    }
+
+    // -------------------------------------------------------
+    //                     iOS SETUP
+    // -------------------------------------------------------
+    private static AppiumDriver setupIOS() throws Exception {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        caps.setCapability("platformName", configReader.get("platformName"));
+        caps.setCapability("appium:automationName", configReader.get("automationName"));
+        caps.setCapability("appium:deviceName", configReader.get("deviceName"));
+        caps.setCapability("appium:udid", configReader.get("uuid"));
+        caps.setCapability("appium:platformVersion", configReader.get("platformVersion"));
+        caps.setCapability("appium:autoAcceptAlerts", Boolean.parseBoolean(configReader.get("autoAcceptAlerts")));
+        caps.setCapability("appium:noReset", false);
+        caps.setCapability("appium:fullReset", false);
+
+        // iOS App: app (.ipa) or bundleId
+        if (configReader.get("app") != null && !configReader.get("app").isEmpty()) {
+            caps.setCapability("appium:app", configReader.get("app"));
+        } else {
+            caps.setCapability("appium:bundleId", configReader.get("bundleId"));
+        }
+
+        return new IOSDriver(new URL(configReader.get("appiumServerURL")), caps);
     }
 }
---
+++ src/main/Java/basePackage/simulatorManager.java
@@ -0,0 +1,26 @@
+package basePackage;
+
+import java.io.IOException;
+
+public class simulatorManager {
+    public static Process appiumProcess;
+
+    public static void openSimulator(){
+        try {
+            appiumProcess=Runtime.getRuntime().exec(new String[]
+                    {"/bin/bash","-c","open -a simulator"});
+            appiumProcess.waitFor();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void closingSimulator(){
+        try {
+            appiumProcess=Runtime.getRuntime().exec(new String[]{"/bin/bash","-c","xcrun simctl shutdown all"});
+            appiumProcess.waitFor();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
