3afeb27293b8e1eb85c87054d4c55a53606ca8b4
KishanVishwas <kishanshetty312@gmail.com>
2025-11-28 18:06:22 +0530

    added DS setup flow

--- src/main/Java/locaters/elementLocators.java
+++ src/main/Java/locaters/elementLocators.java
@@ -162,4 +162,17 @@ public class elementLocators {
 
     @AndroidFindBy(xpath = "//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[3]/android.view.View/android.view.View")
     private WebElement startSJCTA;
-}
+
+    @AndroidFindBy(xpath = "(//android.widget.TextView[@text=\"Daily Saving\"])[2]")
+    private WebElement dsInProfile;
+
+    @AndroidFindBy(xpath = "new UiSelector().className(\"android.widget.EditText\")")
+    private WebElement dsAmountArea;
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text=\"Setup Daily Savings\"]")
+    private WebElement setupDsCTA;
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text=\"Proceed for Payment\"]")
+    private WebElement proceedForPaymentCTA;
+
+    }
---
+++ src/main/Java/pageObjectModel/dailySavingsPOM.java
@@ -0,0 +1,37 @@
+package pageObjectModel;
+
+import locaters.elementLocators;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.WebElement;
+
+import static basePackage.driverFactory.driver;
+import static utilsPackage.waitUtils.*;
+
+@Slf4j
+public class dailySavingsPOM {
+    elementLocators loc = new elementLocators(driver);
+
+    public void setupDailySaving() {
+        waitForVisibility(loc.getProfileIcon()).click();
+        scrollUntilElementFound(driver, loc.getDsInProfile());
+        try {
+            waitForVisibility(loc.getDsInProfile()).click();
+        } catch (Exception e) {
+            log.info("Daily savings option CTA is not clickable");
+        }
+        WebElement dstextArea = waitForVisibility(loc.getDsAmountArea());
+        if (dstextArea.isEnabled()) {
+            dstextArea.clear();
+            dstextArea.sendKeys("200");
+        }
+        waitForClick(loc.getSetupDsCTA()).click();
+        WebElement proceedCTA = waitForVisibility(loc.getProceedForPaymentCTA());
+        if (proceedCTA.isEnabled()) {
+            proceedCTA.click();
+        }
+        waitForVisibility(loc.getPhonePayPaymentButton()).click();
+        waitForClick(loc.getPinCompleted()).click();
+
+        waitForVisibility(loc.getGoToHomeCTA()).click();
+    }
+}
