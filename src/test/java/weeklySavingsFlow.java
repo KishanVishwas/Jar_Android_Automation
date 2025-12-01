180a482a003dad8bb2d851b9f7bb15111a4cde55
Karthikhk1720 <karthikhk172000@gmail.com>
2025-12-01 17:03:41 +0530

    add weekly saving

--- src/test/java/weeklySavingsFlow.java
+++ src/test/java/weeklySavingsFlow.java
@@ -7,6 +7,8 @@ public class weeklySavingsFlow {
     public void weeklySavings() throws InterruptedException{
         WeeklySaving WS=new WeeklySaving();
         WS.setWeeklySaving("100");
+        WS.pauseWeeklySaving();
+        WS.ResumeWeeklySaving();
         WS.stopWeeklySaving();
     }
 }
